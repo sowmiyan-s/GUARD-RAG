485d75cffc3c8d69514f6a1b4ca40a4484027f6e
sowmian_s <sowmisowmiyan58@gmail.com>
2026-06-26 23:28:06 +0530

    chore: fix SPDX license format and remove duplicate license in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
     long_description_content_type="text/markdown",
     author="Sowmiyan S",
     author_email="sowmisowmiyan58@gmail.com",
-    license="MIT",
     url="https://github.com/sowmiyan-s/GUARD-RAG",
     project_urls={
         "Documentation": "https://github.com/sowmiyan-s/GUARD-RAG#readme",
